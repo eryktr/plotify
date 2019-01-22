6e96644a0e65653a54fc56c60b0900c19b2d5ae2
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-22 23:04:59 +0100

    Client now properly sends AJAX requests and receives appropriate responses.

--- plotify/static/js/plot.js
+++ plotify/static/js/plot.js
@@ -1,8 +1,40 @@
-$(document).ready( () => {
+"use strict"
+let chart = require('chart.js');
+
+
+$(document).ready(() => {
     let plotBtn = $("#plot_btn");
     plotBtn.click(function () {
         let expression = $("#function1").val();
-        window.location.replace('rest/'+expression);
-    });
+        let points = [];
 
-});
+        $.ajax({
+            url: "/rest/"+expression, success: function (result) {
+                points = result;
+                points.forEach((point) => console.log(point));
+
+                let ctx = document.getElementById("plot_canvas");
+                let myChart = new Chart(ctx, {
+                    type: 'scatter',
+                    data: {
+                        datasets: [{
+                            label: expression,
+                            data: points
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                type: 'linear',
+                                position: 'bottom'
+                            }]
+                        }
+                    }
+                });
+
+            }
+        });
+
+    });
+})
+;
