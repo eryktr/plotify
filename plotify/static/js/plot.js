0703357a7aee7bf5e78e4858f4247afb062ea96f
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-22 01:14:03 +0100

    Added element IDs in the home template as well as a dummy functionality.

--- plotify/static/js/plot.js
+++ plotify/static/js/plot.js
@@ -1,1 +1,8 @@
-alert("I work");
+$(document).ready( () => {
+    let plotBtn = $("#plot_btn");
+    plotBtn.click(function () {
+        let expression = $("#function1").val();
+        alert(expression);
+    });
+
+});
